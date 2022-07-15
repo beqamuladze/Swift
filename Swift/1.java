75764b5c0dc39b0c9b35f94387f19d8fe5d250f9
beqamuladze <86362504+beqamuladze@users.noreply.github.com>
2022-07-15 14:17:19 +0400

    Add files via upload

---
+++ Swift/1.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("abba"));
+    }
+
+    static boolean isPalindrome(String text){
+        for (int i=0; i<text.length()/2; i++){
+            if (text.charAt(i) != text.charAt(text.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
